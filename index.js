aa35f4e10868d2835be2f811ff9daac904c700b8
Pragya Anand <anandpragya752@gmail.com>
2022-11-07 14:46:33 +0530

    navbar,slider,body structure completed

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
-var swiper = new Swiper(".bg-slider", {
-    loop: true,
-    spaceBetween: 10,
-    slidesPerView: 4,
-    freeMode: true,
-    watchSlidesProgress: true,
-  });
-  var swiper2 = new Swiper(".bg-slider", {
-    loop: true,
-    spaceBetween: 10,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    thumbs: {
+var swiper = new Swiper(".bg-slider-thumbs", {
+  loop: true,
+  spaceBetween: 0,
+  slidesPerView: 0,
+});
+var swiper2 = new Swiper(".bg-slider", {
+  loop: true,
+  spaceBetween: 0,
+  thumbs: {
       swiper: swiper,
-    },
-  });
+  },
+
+  //nav bar effects
+});
+window.addEventListener('scroll',function(){
+  const header = this.document.querySelector('header');
+  header.classList.toggle('sticky',window.scrollY>1)
+});
+
+//nav menu bar toggle
+const menubtn = querySelector('.nav-menu-btn');
+const closebtn = querySelector('.nav-close-btn');
+const navigation = querySelector('.navigation');
+
+menubtn.addEventListener('click',()=>{
+  navigation.classList.add("active");
+})
+closebtn.addEventListener('click',()=>{
+  navigation.classList.remove("active")
+})
